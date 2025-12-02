811f6e6ad9df1b4f88cdc6c25f2bc983489263f7
Joseph Dillard <greenjodillard@gmail.com>
2025-12-02 15:50:31 -0600

    updated with validation hashing

--- backend/helpers/pass_hash.py
+++ backend/helpers/pass_hash.py
@@ -2,6 +2,10 @@ from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
 
-def pass_hash(password: str):
+def pass_hash(password):
     hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
     return hashed_password
+
+def check_pass(hashedpass, password):
+    valid_pass = bcrypt.check_password_hash(hashedpass, password)
+    return valid_pass
